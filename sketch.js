b5b3fa7bc758ac55498f01d3c62353d3c6bd9414
AmalaSheefa <74779154+AmalaSheefa@users.noreply.github.com>
2021-09-23 06:26:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+
+var trex ,trex_running;
+
+
+function preload(){
+  
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  trex = createSprite(50,160,20,50);
+  
+}
+
+function draw(){
+  background("white")
+  
+drawSprites();
+}
